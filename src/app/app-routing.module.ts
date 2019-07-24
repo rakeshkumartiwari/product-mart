a3b0d8ac9b48885cee44353a8e23068c7c42cc3b
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2019-07-24 10:14:42 +0530

    adding toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    pathMatch: 'full',
     component: HomeComponent
   },
   {
