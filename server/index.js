9f5f8ac208def00b5daaa194661595285cbf2c5e
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2019-08-01 09:13:01 +0530

    local hosting our production code

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT | 4260;
+const app = express();
+
+const destinationDir = path.join(__dirname, '../dist')
+
+//hosting from dist folder
+app.use(express.static(destinationDir))
+console.log(`express hosting from ${destinationDir} `)
+
+//serving index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(destinationDir, 'index.html'))
+})
+console.log('serving index.html')
+
+//initialize app and listen to port
+app.listen(port, () => { console.log(`server is running from port ${port}`) })
+
+
